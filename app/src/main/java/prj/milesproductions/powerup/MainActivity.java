07b652fcd5f211266967cae6bb522253dd014f1e
aidan0203 <aidan0203@users.noreply.github.com>
2017-08-15 15:29:12 +0800

    Update MainActivity.java

--- app/src/main/java/prj/milesproductions/powerup/MainActivity.java
+++ app/src/main/java/prj/milesproductions/powerup/MainActivity.java
@@ -60,9 +60,7 @@ public class MainActivity extends AppCompatActivity {
         txtTemperature = (TextView) findViewById(R.id.txtTemperature);
         txtState = (TextView) findViewById(R.id.txtState);
         txtHealth = (TextView) findViewById(R.id.txtHealth);
-
         btnBatteryUsage = (Button) findViewById(R.id.btnBatteryUsage);
-
         mGauge = (ProgressBar) findViewById(R.id.mGauge);
 
         mTypeface = Typeface.createFromAsset(getAssets(), "fonts/Calibre-Bold.otf");
@@ -79,144 +77,28 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        getDeviceName();
-        getBatteryData();
-        setBatteryData();
+        mDevice = getDeviceName();
+        if(mDevice == null) {
+            System.exit(1);
+        }
+
+        getAndSetBatteryData();
         animateGauge();
 
         final Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                getBatteryData();
-                setBatteryData();
+                getAndSetBatteryData();
                 handler.postDelayed(this, 2000);
             }
         }, 2000);
     }
 
-    private void animateGauge() {
-        ObjectAnimator animGauge = ObjectAnimator.ofInt(mGauge, "progress", 0, capacity);
-        animGauge.setDuration(1000);
-        animGauge.setInterpolator(new DecelerateInterpolator());
-        animGauge.start();
-
-        mGauge.clearAnimation();
-    }
-
-    public void getDeviceName() {
-        try {
-            Process px = Runtime.getRuntime().exec("getprop ro.product.device");
-            BufferedReader pxb = new BufferedReader(new InputStreamReader(px.getInputStream()));
-            mDevice = pxb.readLine();
-            Log.i(TAG, "Gathered device model: " + mDevice);
-        } catch (IOException e) {
-            Log.e(TAG, "Unable to gather device model, exiting...");
-            System.exit(0);
-        }
-    }
-
-    public void getBatteryData() {
-        if(mDevice.equals("ExcitePrime")) {
-            try {
-                float   prop_charge_full        = 2820.0f;
-                float   prop_capacity           = Float.parseFloat(FileUtils.fileRead("/sys/class/power_supply/battery/capacity").trim());
-                float   prop_temp               = Float.parseFloat(FileUtils.fileRead("/sys/class/power_supply/battery/batt_temp").trim());
-                String  prop_status             = FileUtils.fileRead("/sys/class/power_supply/battery/status").trim();
-                String  prop_health             = FileUtils.fileRead("/sys/class/power_supply/battery/health").trim();
-                float   prop_voltage            = Float.parseFloat(FileUtils.fileRead("/sys/class/power_supply/battery/BatterySenseVoltage").trim());
-                int     prop_current_charging   = Integer.parseInt(FileUtils.fileRead("/sys/class/power_supply/battery/BatteryAverageCurrent").trim());
-                long    prop_current_discharging= Long.parseLong(FileUtils.fileRead("/sys/class/power_supply/battery/device/FG_Battery_CurrentConsumption").trim());
-
-                charge_now = Math.round(prop_charge_full * (prop_capacity / 100f));
-                charge_full = (int) prop_charge_full;
-                capacity = (int) prop_capacity;
-                temp = prop_temp / 10;
-                voltage = prop_voltage / 1000;
-                status = prop_status;
-                health = prop_health;
-
-                if(status.equals("Charging") || status.equals("Full")) {
-                    current = prop_current_charging * 2;
-                } else {
-                    if(prop_current_discharging > 40000000) {
-                        current = 0;
-                    } else {
-                        current = (int) prop_current_discharging / -10;
-                    }
-                }
-
-            } catch(IOException e) {
-                Log.e(TAG, "IOException: " + e.toString());
-            }
-        } else if(mDevice.equals("FlareS")) {
-            try {
-                float   prop_charge_full        = 1400.0f;
-                float   prop_capacity           = Float.parseFloat(FileUtils.fileRead("/sys/class/power_supply/battery/capacity").trim());
-                float   prop_temp               = Float.parseFloat(FileUtils.fileRead("/sys/class/power_supply/battery/batt_temp").trim());
-                String  prop_status             = FileUtils.fileRead("/sys/class/power_supply/battery/status").trim();
-                String  prop_health             = FileUtils.fileRead("/sys/class/power_supply/battery/health").trim();
-                float   prop_voltage            = Float.parseFloat(FileUtils.fileRead("/sys/class/power_supply/battery/BatterySenseVoltage").trim());
-                int     prop_current_charging   = Integer.parseInt(FileUtils.fileRead("/sys/class/power_supply/battery/BatteryAverageCurrent").trim());
-                long    prop_current_discharging= Long.parseLong(FileUtils.fileRead("/sys/class/power_supply/battery/device/FG_Battery_CurrentConsumption").trim());
-
-                charge_now = Math.round(prop_charge_full * (prop_capacity / 100f));
-                charge_full = (int) prop_charge_full;
-                capacity = (int) prop_capacity;
-                temp = prop_temp / 10;
-                voltage = prop_voltage / 1000;
-                status = prop_status;
-                health = prop_health;
-
-                if(status.equals("Charging") || status.equals("Full")) {
-                    current = prop_current_charging;
-                } else {
-                    if(prop_current_discharging > 40000000) {
-                        current = 0;
-                    } else {
-                        current = (int) prop_current_discharging / -10;
-                    }
-                }
-
-            } catch(IOException e) {
-                Log.e(TAG, "IOException: " + e.toString());
-            }
-        } else {
-            Log.e(TAG, "Exiting. Device not supported.");
-            System.exit(1);
-        }
-    }
-
-    public void setBatteryData() {
-        if(temp >= 45) {
-            txtTemperature.setTextColor(Color.parseColor("#FF0000"));
-        } else {
-            txtTemperature.setTextColor(Color.parseColor("#666666"));
-        }
-
-        tempf = (int) (((temp * 9) / 5) + 32);
-        if(status.equals("Charging") && current < 0) {
-            txtCurrent.setTextColor(Color.parseColor("#FF0000"));
-        } else {
-            txtCurrent.setTextColor(Color.parseColor("#666666"));
-        }
-
-        txtState.setText(status);
-        txtHealth.setText(health);
-        txtCurrent.setText(current + " mA");
-        txtCapacity.setText(capacity + "%");
-        txtCharge.setText(charge_now + " mAh");
-        txtChargeTotal.setText(charge_full + " mAh");
-        txtVoltage.setText(voltage + " V");
-        txtTemperature.setText(temp + " °C (" + tempf + " °F)");
-
-        mGauge.setProgress(capacity);
-    }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         boolean s = isMyServiceRunning(AmpereMeterService.class);
-        if(s == false) {
+        if(!s) {
             getMenuInflater().inflate(R.menu.main_svcstart, menu);
         } else {
             getMenuInflater().inflate(R.menu.main_svcstop, menu);
@@ -237,7 +119,7 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, "Service stopped", Toast.LENGTH_LONG).show();
             stopService(new Intent(MainActivity.this, AmpereMeterService.class));
             NotificationManager mNotifyMgr = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-            mNotifyMgr.cancel(001);
+            mNotifyMgr.cancelAll();
             invalidateOptionsMenu();
         }
         if(selectedItem == R.id.ABOUT) {
@@ -281,6 +163,140 @@ public class MainActivity extends AppCompatActivity {
             }
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public String getDeviceName() {
+        try {
+            Process px = Runtime.getRuntime().exec("getprop ro.product.device");
+            BufferedReader pxb = new BufferedReader(new InputStreamReader(px.getInputStream()));
+            return pxb.readLine();
+        } catch (IOException e) {
+            Log.e(TAG, "Unable to gather device model, exiting...");
+            return null;
+        }
+    }
+
+    private void getAndSetBatteryData() {
+        getBatteryData();
+        setBatteryData();
+    }
+
+    public void getBatteryData() {
+        try {
+            if (mDevice.equals("ExcitePrime")) {
+                float prop_charge_full = 2820.0f;
+                float prop_capacity = Float.parseFloat(FileUtils.fileRead("/sys/class/power_supply/battery/capacity").trim());
+                float prop_temp = Float.parseFloat(FileUtils.fileRead("/sys/class/power_supply/battery/batt_temp").trim());
+                String prop_status = FileUtils.fileRead("/sys/class/power_supply/battery/status").trim();
+                String prop_health = FileUtils.fileRead("/sys/class/power_supply/battery/health").trim();
+                float prop_voltage = Float.parseFloat(FileUtils.fileRead("/sys/class/power_supply/battery/BatterySenseVoltage").trim());
+                int prop_current_charging = Integer.parseInt(FileUtils.fileRead("/sys/class/power_supply/battery/BatteryAverageCurrent").trim());
+                long prop_current_discharging = Long.parseLong(FileUtils.fileRead("/sys/class/power_supply/battery/device/FG_Battery_CurrentConsumption").trim());
+
+                charge_now = Math.round(prop_charge_full * (prop_capacity / 100f));
+                charge_full = (int) prop_charge_full;
+                capacity = (int) prop_capacity;
+                temp = prop_temp / 10;
+                voltage = prop_voltage / 1000;
+                status = prop_status;
+                health = prop_health;
+
+                if (status.equals("Charging") || status.equals("Full")) {
+                    current = prop_current_charging * 2;
+                } else {
+                    if (prop_current_discharging > 40000000) {
+                        current = 0;
+                    } else {
+                        current = (int) prop_current_discharging / -10;
+                    }
+                }
+            } else if (mDevice.equals("FlareS")) {
+                float prop_charge_full = 1400.0f;
+                float prop_capacity = Float.parseFloat(FileUtils.fileRead("/sys/class/power_supply/battery/capacity").trim());
+                float prop_temp = Float.parseFloat(FileUtils.fileRead("/sys/class/power_supply/battery/batt_temp").trim());
+                String prop_status = FileUtils.fileRead("/sys/class/power_supply/battery/status").trim();
+                String prop_health = FileUtils.fileRead("/sys/class/power_supply/battery/health").trim();
+                float prop_voltage = Float.parseFloat(FileUtils.fileRead("/sys/class/power_supply/battery/BatterySenseVoltage").trim());
+                int prop_current_charging = Integer.parseInt(FileUtils.fileRead("/sys/class/power_supply/battery/BatteryAverageCurrent").trim());
+                long prop_current_discharging = Long.parseLong(FileUtils.fileRead("/sys/class/power_supply/battery/device/FG_Battery_CurrentConsumption").trim());
+
+                charge_now = Math.round(prop_charge_full * (prop_capacity / 100f));
+                charge_full = (int) prop_charge_full;
+                capacity = (int) prop_capacity;
+                temp = prop_temp / 10;
+                voltage = prop_voltage / 1000;
+                status = prop_status;
+                health = prop_health;
+
+                if (status.equals("Charging") || status.equals("Full")) {
+                    current = prop_current_charging;
+                } else {
+                    if (prop_current_discharging > 40000000) {
+                        current = 0;
+                    } else {
+                        current = (int) prop_current_discharging / -10;
+                    }
+                }
+            } else if (mDevice.equals("ASUS_T00I")) {
+                float prop_charge_now = Float.parseFloat(FileUtils.fileRead("/sys/class/power_supply/battery/charge_now").trim());
+                float prop_charge_full = Float.parseFloat(FileUtils.fileRead("/sys/class/power_supply/battery/charge_full").trim());
+                float prop_capacity = Float.parseFloat(FileUtils.fileRead("/sys/class/power_supply/battery/capacity").trim());
+                float prop_temp = Float.parseFloat(FileUtils.fileRead("/sys/class/power_supply/battery/temp_ambient").trim());
+                String prop_status = FileUtils.fileRead("/sys/class/power_supply/battery/status").trim();
+                String prop_health = FileUtils.fileRead("/sys/class/power_supply/battery/health").trim();
+                float prop_voltage = Float.parseFloat(FileUtils.fileRead("/sys/class/power_supply/battery/voltage_now").trim());
+                int prop_current = Integer.parseInt(FileUtils.fileRead("/sys/class/power_supply/battery/current_now").trim());
+
+                charge_now = (int) prop_charge_now;
+                charge_full = (int) prop_charge_full;
+                capacity = (int) prop_capacity;
+                temp = prop_temp / 10;
+                voltage = prop_voltage / 1000;
+                status = prop_status;
+                health = prop_health;
+                current = prop_current;
+            } else {
+                Log.e(TAG, "Exiting. Device not supported.");
+                System.exit(1);
+            }
+        } catch (IOException e) {
+
+        }
+    }
+
+    public void setBatteryData() {
+        if(temp >= 45) {
+            txtTemperature.setTextColor(Color.parseColor("#FF0000"));
+        } else {
+            txtTemperature.setTextColor(Color.parseColor("#666666"));
+        }
+
+        tempf = (int) (((temp * 9) / 5) + 32);
+        if(status.equals("Charging") && current < 0) {
+            txtCurrent.setTextColor(Color.parseColor("#FF0000"));
+        } else {
+            txtCurrent.setTextColor(Color.parseColor("#666666"));
+        }
+
+        txtState.setText(status);
+        txtHealth.setText(health);
+        txtCurrent.setText(current + " mA");
+        txtCapacity.setText(capacity + "%");
+        txtCharge.setText(charge_now + " mAh");
+        txtChargeTotal.setText(charge_full + " mAh");
+        txtVoltage.setText(voltage + " V");
+        txtTemperature.setText(temp + " °C (" + tempf + " °F)");
+
+        mGauge.setProgress(capacity);
+    }
+
+    private void animateGauge() {
+        ObjectAnimator animGauge = ObjectAnimator.ofInt(mGauge, "progress", 0, capacity);
+        animGauge.setDuration(1000);
+        animGauge.setInterpolator(new DecelerateInterpolator());
+        animGauge.start();
+
+        mGauge.clearAnimation();
     }
 
     private boolean isMyServiceRunning(Class<?> serviceClass) {
